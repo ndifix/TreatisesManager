ad4bdf174316dabf3340802c7ef7a74314880d84
ndifix <yoshiki.linuxmint@gmail.com>
2022-06-26 14:04:19 +0900

    リストにファイルを追加して表示できるようにした

--- Command/MainWindow/OpenFileCommand.cs
+++ Command/MainWindow/OpenFileCommand.cs
@@ -1,22 +1,44 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using TreatisesManager.Model;
 
-namespace Treatises.Command.MainWindow
+namespace TreatisesManager.Command.MainWindow
 {
 	class OpenFileCommand : ICommand
 	{
 		public event EventHandler CanExecuteChanged;
 
+		private readonly ObservableCollection<Treatise> treatises;
+
+		public OpenFileCommand(ObservableCollection<Treatise> treatises)
+		{
+			this.treatises = treatises;
+		}
+
 		public bool CanExecute(object parameter) => true;
 
 		public void Execute(object parameter)
 		{
-			MessageBox.Show("show");
+			var dialog = new OpenFileDialog();
+			var treatise = new Treatise()
+			{
+				Title = "",
+				Authors = "",
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				treatise.FilePath = dialog.FileName;
+			}
+
+			treatises.Add(treatise);
 		}
 	}
 }
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TreatisesManager.Command.MainWindow;
 using TreatisesManager.Model;
 
 namespace TreatisesManager.ViewModel
 {
 	class MainWindowViewModel
 	{
-		public List<Treatise> Treatises { get; }
+		public ObservableCollection<Treatise> Treatises { get; }
+
+		public ICommand OpenFileCommand { get; }
 
 		public MainWindowViewModel()
 		{
-			Treatises = new List<Treatise>();
+			Treatises = new ObservableCollection<Treatise>();
+			OpenFileCommand = new OpenFileCommand(Treatises);
 		}
 	}
 }
